d516c2d63e7cf90bb17383e7366fcf1b9dcdd278
Jeff Maher <jeff.maher@cds-snc.ca>
2019-11-23 17:24:20 -0500

    Configured for Nunjucks

--- config/views.js
+++ config/views.js
@@ -16,26 +16,10 @@
 
 module.exports.views = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Extension to use for your views. When calling `res.view()` in an action, *
-  * you can leave this extension off. For example, calling                   *
-  * `res.view('homepage')` will (using default settings) look for a          *
-  * `views/homepage.ejs` file.                                               *
-  *                                                                          *
-  ***************************************************************************/
-
-  // extension: 'ejs',
-
-  /***************************************************************************
-  *                                                                          *
-  * The path (relative to the views directory, and without extension) to     *
-  * the default layout file to use, or `false` to disable layouts entirely.  *
-  *                                                                          *
-  * Note that layouts only work with the built-in EJS view engine!           *
-  *                                                                          *
-  ***************************************************************************/
-
-  layout: 'layouts/layout'
+  extension: 'njk',
+  getRenderFn: ()=>{
+    var cons = require('consolidate')
+    return cons.nunjucks
+  }
 
 };
