1ec8e05a777be9de7b964dbe29f45d815a0afada
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-12-06 15:25:22 -0500

    copy files from assets to public

--- config/webpack.js
+++ config/webpack.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports.webpack = {
@@ -10,7 +11,23 @@ module.exports.webpack = {
 
       plugins: [
         new webpack.ProgressPlugin(),
+
+        // This plugin cleans out your .tmp/public folder before lifting.
         new CleanWebpackPlugin(),
+
+        // This plugin copies the `images` and `fonts` folders into
+        // the .tmp/public folder.  You can add any other static asset
+        // folders to this list and they'll be copied as well.
+        new CopyWebpackPlugin([
+          {
+            from: "./assets/img",
+            to: path.resolve(__dirname, "..", ".tmp", "public", "img")
+          }
+          /* {
+            from: "./assets/fonts",
+            to: path.resolve(__dirname, "..", ".tmp", "public", "fonts")
+          } */
+        ]),
         new MiniCssExtractPlugin({
           filename: "./assets/styles/app.css"
         })
