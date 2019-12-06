d39e4d335fa7982aa8e90788f0bf847d1537d375
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-12-06 17:09:41 -0500

    add home page controller

---
+++ api/controllers/StartController.js
@@ -0,0 +1,12 @@
+/**
+ * StartController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  index: function(req, res) {
+    res.view("pages/homepage");
+  }
+};
