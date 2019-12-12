37672c4df7f6da2420be546a209c11ef4cdb05c5
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-12-12 17:57:57 -0500

    another working-messy version

--- api/controllers/PersonalController.js
+++ api/controllers/PersonalController.js
@@ -1,3 +1,5 @@
+const validate = require('validate.js');
+
 /**
  * PersonalController
  *
@@ -5,12 +7,34 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+
 module.exports = {
   index: function (req, res) {
     res.view("pages/personal");
   },
 
   store: function (req, res) {
+
+    const validations = {
+      social: {
+        presence: true,
+        numericality: {
+          message: "Must be numeric"
+        }
+      }
+    }
+
+    const result = validate(req.body, validations);
+    if (result) {
+      sails.log.info(result);
+      req.flash('errors', result);
+      return res.redirect('back');
+    } else {
+      return res.redirect('start');
+    }
+
+
+    /*
     req.validate({
       'social': 'numeric'
     }, (err, params) => {
@@ -24,5 +48,6 @@ module.exports = {
         res.redirect('/en/start');
       }
     });
+    */
   }
 };
