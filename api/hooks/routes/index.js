5e21e440f3e4958ac71902f13a92812ca1455f14
Dave Samojlenko <dsamojlenko@gmail.com>
2019-12-11 21:40:11 -0500

    move route helper to hook

--- api/hooks/routes/index.js
+++ api/hooks/routes/index.js
@@ -53,6 +53,46 @@ module.exports = sails => {
         initialize(done) {
             sails.log.info("Initializing custom hook (`routes`)")
 
+            sails.route = (routeName, params = {}) => {
+                let lang = sails.hooks.i18n.getLocale();
+
+                // use optional lang param if passed
+                if (_.has(params, 'lang')) {
+                    lang = params.lang;
+                }
+
+                // get i18n routes
+                const routes = sails.router.namedRoutes[routeName]
+
+                if (!routes) {
+                    throw new Error('No named route : ' + routeName + '\n Maybe you missed the name property?')
+                }
+
+                // get localized route
+                let route = routes[lang]
+
+                if (!route) {
+                    throw new Error('No named route : ' + routeName + '\n in that language. Maybe you mistyped the lang property?')
+                }
+
+                // map any passed-in params to placeholders in the route path
+                if (!_.isEmpty(params)) {
+                    Object.keys(params).forEach((key, index) => {
+                        route = route.replace(':' + key, params[key])
+                    })
+                }
+
+                return route
+            }
+
+            sails.on('router:before', () => {
+                sails.router.bind('all /*', (req, res, next) => {
+                    res.locals.route = sails.route
+
+                    return next()
+                })
+            })
+
             return done()
         }
     }
