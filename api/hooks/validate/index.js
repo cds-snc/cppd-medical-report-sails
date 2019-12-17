6bc741ecbeb0c0902d5b72d2295d1beeffce6323
Dave Samojlenko <dsamojlenko@gmail.com>
2019-12-17 12:06:29 -0500

    add some comments and flash data

--- api/hooks/validate/index.js
+++ api/hooks/validate/index.js
@@ -7,6 +7,40 @@ const validate = require('validate.js');
  * @docs        :: https://sailsjs.com/docs/concepts/extending-sails/hooks
  */
 
+
+/*
+  We need to flatten the array object from Validate.js
+  ie,
+  [ 
+    { 
+      social:
+      [ 
+        'Social must be numeric',
+        'Social needs to have 9 digits or more' 
+      ],
+      first_name: 
+      [ 
+        'First name can\'t be blank' 
+      ],
+      last_name: 
+      [ 
+        'Last name can\'t be blank' 
+      ] 
+    } 
+  ]
+  to
+  {
+    social: { param: 'social', msg: 'Must be numeric', ... },
+    social: { param: 'social', msg: 'needs to have 9 digits or more', ... },
+    first_name: { param: 'first_name', msg: 'Cannot be empty', ... },
+    last_name: { param: 'last_name', msg: 'Cannot be empty', ... },
+  }
+*/ 
+const formatErrorObject = (errors = []) => {
+
+}
+
+
 module.exports = function defineValidateHook(sails) {
 
   return {
@@ -19,14 +53,14 @@ module.exports = function defineValidateHook(sails) {
       sails.log.info('Initializing custom hook (`validate`)');
 
       sails.validate = (req, res, validations) => {
-        const result = validate(req.body, validations);
+        let errors = validate(req.body, validations);
 
         /** 
          * if there are validation errors, flash them along
          * with the data to the session and redirect back
          */
-        if (result) {
-          req.flash('errors', result);
+        if (errors) {
+          req.flash('errors', errors);
           req.flash('data', req.body);
           return res.redirect('back');
         }
