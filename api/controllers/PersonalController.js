5bfa875808b940aaaf0fd966dc44a8ea07f18a7a
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-12-17 13:35:20 -0500

    Add validation library and helper (#4)
    * checking in a working-ugly version
    
    * another working-messy version
    
    * create validate function hook
    
    * add some more validation and make it work
    
    * add some comments and flash data
    
    * remove unused method for now
    
    * make some changes to accommodate new error object

--- api/controllers/PersonalController.js
+++ api/controllers/PersonalController.js
@@ -5,13 +5,53 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+
 module.exports = {
   index: function (req, res) {
-    res.view("pages/personal");
+    /**
+     * If there are errors or data flashed to the session
+     * assign them to local variables
+     */
+    const errors = req.flash('errors');
+    const data = req.flash('data')[0];
+
+    res.view('pages/personal', {
+      errors: errors.length ? errors : null,
+      data: data
+    });
   },
 
   store: function (req, res) {
-    req.flash('errors', ['Huzzah']);
-    res.redirect('/en/personal');
+    let valid = req.validate(req, res, {
+      social: {
+        presence: true,
+        numericality: {
+          message: '^Social must be numeric'
+        },
+        length: {
+          minimum: 9,
+          maximum: 9,
+          tooShort: "^Social needs to have %{count} digits or more",
+          tooLong: "^Whoa that's too much"
+        }
+      },
+      first_name: {
+        presence: {
+          allowEmpty: false,
+        },
+      },
+      last_name: {
+        presence: {
+          allowEmpty: false
+        }
+      }
+    })
+
+    if (valid) {
+      // probably save the model here
+      // optionally flash a success message
+      // then redirect
+      res.redirect('/en/start');
+    }
   }
 };
---
+++ api/hooks/validate/index.js
@@ -0,0 +1,50 @@
+const validate = require('validate.js');
+
+/**
+ * validate hook
+ *
+ * @description :: A hook definition.  Extends Sails by adding shadow routes, implicit actions, and/or initialization logic.
+ * @docs        :: https://sailsjs.com/docs/concepts/extending-sails/hooks
+ */
+
+module.exports = function defineValidateHook(sails) {
+
+  return {
+
+    /**
+     * Runs when this Sails app loads/lifts.
+     */
+    initialize: async function (done) {
+
+      sails.log.info('Initializing custom hook (`validate`)');
+
+      sails.validate = (req, res, validations) => {
+        let errors = validate(req.body, validations);
+
+        /** 
+         * if there are validation errors, flash them along
+         * with the data to the session and redirect back
+         */
+        if (errors) {
+          req.flash('errors', errors);
+          req.flash('data', req.body);
+          return res.redirect('back');
+        }
+        return true;
+      }
+
+      /**
+       * Bind the validate method to the req object so it's
+       * available in our Controllers.
+       */
+      sails.on('router:before', () => {
+        sails.router.bind('all /*', (req, res, next) => {
+          req.validate = sails.validate
+
+          return next()
+        })
+      })
+      return done()
+    }
+  };
+};
