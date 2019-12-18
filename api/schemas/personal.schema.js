70beb0e66f195992d657798e94605547d6fcb78f
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-12-18 16:58:02 -0500

    flesh out validation on personal info form

--- api/schemas/personal.schema.js
+++ api/schemas/personal.schema.js
@@ -1,30 +1,67 @@
 module.exports = {
-    social: {
-        presence: true,
-        numericality: {
-            message: '^Social must be numeric'
-        },
-        length: {
-            minimum: 9,
-            maximum: 9,
-            tooShort: "^Social needs to have %{count} digits or more",
-            tooLong: "^Whoa that's too much"
-        }
+  social: {
+    presence: {
+      allowEmpty: false,
+      message: '^Social Insurance Number is required'
     },
-    first_name: {
-        presence: {
-            allowEmpty: false,
-        },
+    validateSIN: {
+      message: '^Social Insurance Number is invalid'
     },
-    last_name: {
-        presence: {
-            allowEmpty: false
-        }
+  },
+  first_name: {
+    presence: {
+      allowEmpty: false,
+      message: '^First name is required'
     },
-    preferred_title: {
-        presence: true
-    },
-    contact_time: {
-        presence: true
+  },
+  last_name: {
+    presence: {
+      allowEmpty: false,
+      message: '^Last name is required'
     }
+  },
+  birthdate: {
+    presence: {
+      allowEmpty: false,
+      message: '^Birthdate is required'
+    },
+    validateDateFormat: {
+      message: '^Birthdate is not formatted correctly'
+    },
+    validateDateExists: {
+      message: '^Birthdate is not a valid date'
+    }
+  },
+  address: {
+    presence: {
+      allowEmpty: false,
+      message: '^Mailing address is required'
+    }
+  },
+  telephone: {
+    presence: {
+      allowEmpty: false,
+      message: '^Telephone is required'
+    },
+    format: {
+      pattern: /^(\+0?1\s)??\(?\d{3}\)?[-]\d{3}[-]\d{4}$/,
+      message: '^Telephone is incorrectly formatted'
+    }
+  },
+  alternate_telephone: {
+    format: {
+      pattern: /^(\+0?1\s)??\(?\d{3}\)?[-]\d{3}[-]\d{4}$/,
+      message: '^Alternate telephone is incorrectly formatted'
+    },
+    presence: {
+      allowEmpty: true
+    }
+  },
+  contact_time: {
+    presence: true,
+    inclusion: {
+      within: ['morning', 'afternoon', 'letters_only'],
+      message: '^Invalid selection'
+    }
+  }
 }
