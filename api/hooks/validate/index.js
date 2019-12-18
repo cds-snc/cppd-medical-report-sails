5c357d7876957d40fb284f9a1cc286c569f682fb
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-12-18 16:57:44 -0500

    add custom validators

--- api/hooks/validate/index.js
+++ api/hooks/validate/index.js
@@ -1,5 +1,10 @@
 const validate = require('validate.js');
 
+// load custom validators
+validate.validators.validateSIN = require('./validators/sin.validator').validateSIN;
+validate.validators.validateDateFormat = require('./validators/dates.validator').validateDateFormat;
+validate.validators.validateDateExists = require('./validators/dates.validator').validateDateExists;
+
 /**
  * validate hook
  *
