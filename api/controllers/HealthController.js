c854ae5f68e565597b1df80a80a213975e2defcf
Dave Samojlenko <dsamojlenko@gmail.com>
2019-12-24 12:30:28 -0500

    add schema and save

--- api/controllers/HealthController.js
+++ api/controllers/HealthController.js
@@ -6,7 +6,25 @@
  */
 
 module.exports = {
-  
+  index: function (req, res) {
+    /**
+     * If there is a medical report in the session, load it
+     */
+    if (req.session.medicalReport) {
+      res.locals.data = req.session.medicalReport;
+    }
+
+    res.view('pages/health');
+  },
+
+  store: function (req, res) {
+    let valid = req.validate(req, res, require('../schemas/health.schema'));
+
+    if (valid) {
+      // save the model
+      req.session.medicalReport.overall_health = req.body.overall_health;
+      res.redirect(sails.route('conditions'));
+    }
+  }
 
 };
-
---
+++ api/schemas/health.schema.js
@@ -0,0 +1,8 @@
+module.exports = {
+  overall_health: {
+    presence: {
+      allowEmpty: false,
+      message: '^errors.overall_health_required'
+    }
+  },
+}
