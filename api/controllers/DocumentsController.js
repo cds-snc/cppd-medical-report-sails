0fae4831e0c1d52c03e24f7b9635f2f511ffebd1
Dave Samojlenko <dsamojlenko@gmail.com>
2019-12-24 12:39:09 -0500

    stub out documents section

---
+++ api/controllers/DocumentsController.js
@@ -0,0 +1,16 @@
+/**
+ * DocumentsController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  index: function (req, res) {
+
+  },
+  
+  store: function (req, res) {
+
+  }
+};
