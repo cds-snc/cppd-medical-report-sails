471adc59580ddc1e26e9d58c4453851c2347a5be
Dave Samojlenko <dsamojlenko@gmail.com>
2019-12-24 12:04:49 -0500

    Treatments index

--- api/controllers/TreatmentsController.js
+++ api/controllers/TreatmentsController.js
@@ -7,6 +7,12 @@
 
 module.exports = {
   index: function (req, res) {
+    if (!_.has(req.session.medicalReport, 'treatments')) {
+      res.redirect(sails.route('treatments.add'));
+    } else {
+      res.locals.data = req.session.medicalReport;
 
+      res.view('pages/treatments/index');
+    }
   }
 };
