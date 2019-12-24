b247f45608fcff911a168c97da8da929c28124a6
Dave Samojlenko <dsamojlenko@gmail.com>
2019-12-24 10:56:15 -0500

    add medications

--- api/controllers/AddMedicationController.js
+++ api/controllers/AddMedicationController.js
@@ -5,13 +5,35 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+ const {
+  conditionReducer,
+  oneAttribute,
+} = require('../utils/condition.mapper')
+
 module.exports = {
   create: function (req, res) {
+    const data = req.session.medicalReport
+    const conditionList = conditionReducer(data.conditions)
 
-  }
+    res.view('pages/medications/add', {
+      conditionList: conditionList
+    });
+  },
 
   store: function (req, res) {
+    const body = Object.assign({}, req.body);
+    delete body._csrf;
 
+    let valid = req.validate(req, res, require('../schemas/medication.schema'));
+
+    if (valid) {
+      // save model here
+      if (!_.has(req.session.medicalReport, 'medications')) {
+        req.session.medicalReport.medications = [];
+      }
+      req.session.medicalReport.medications.push(body);
+      res.redirect(sails.route('medications'));
+    }
   }
 };
 
---
+++ api/utils/condition.mapper.js
@@ -0,0 +1,19 @@
+/**
+ * I don't know if we should convert this to a "sails helper"
+ */
+// const reduce = require('lodash.reduce')
+
+const conditionReducer = (conditions) => {
+  return _.reduce(
+    conditions,
+    (outList, condition, index) => {
+      outList[index] = condition.name_of_condition
+      return outList
+    },
+    {},
+  )
+}
+
+const oneAttribute = (obj) => (Object.keys(obj).length === 1)
+
+module.exports =  { conditionReducer, oneAttribute };
