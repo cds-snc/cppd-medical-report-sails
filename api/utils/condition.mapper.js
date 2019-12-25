0786d8329e23d0bc5e32f0f57056d3c7fa65e393
Dave Samojlenko <dsamojlenko@gmail.com>
2019-12-24 21:02:39 -0500

    added more helpful comments

--- api/utils/condition.mapper.js
+++ api/utils/condition.mapper.js
@@ -1,3 +1,24 @@
+/**
+ * Takes an array of conditions of the following structure:
+ * [
+ *    {
+ *       clinically_impair: "",
+ *       condition_last: "",
+ *       condition_outlook: "",
+ *       condition_outlook_unknown: "",
+ *       name_of_condition: "",
+ *       symptoms_began: "",
+ *       symptoms_occur: "",
+ *       symptoms_occur_uknown: ""
+ *    }
+ * ]
+ * ... and returns an object like so:
+ *  {
+ *    0: "asdf",
+ *    1: "test",
+ *    2: "asdfasdf"
+ *  }
+ */
 const conditionReducer = (conditions) => {
   return _.reduce(
     conditions,
