475801a525ca53dedb3bf8e35206e9d64834fb87
Dave Samojlenko <dsamojlenko@gmail.com>
2019-12-27 12:27:27 -0500

    add relationship form, routes, schema

---
+++ api/controllers/RelationshipController.js
@@ -0,0 +1,37 @@
+/**
+ * RelationshipController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  index: function (req, res) {
+    /**
+     * If there is a medical report in the session, load it
+     */
+    if (req.session.medicalReport) {
+      if (res.locals.data) {
+        res.locals.data = _.merge(res.locals.data, req.session.medicalReport);
+      } else {
+        res.locals.data = req.session.medicalReport;
+      }
+    }
+
+    res.view('pages/relationship');
+  },
+
+  store: function (req, res) {
+    let valid = req.validate(req, res, require('../schemas/relationship.schema'));
+
+    if (valid) {
+      // save the model
+      req.session.medicalReport.numYearsWasPatient = req.body.numYearsWasPatient;
+      req.session.medicalReport.visitNumber = req.body.visitNumber;
+      req.session.medicalReport.lastVisitDate = req.body.lastVisitDate;
+      req.session.medicalReport.firstTreatmentDate = req.body.firstTreatmentDate;
+
+      res.redirect(sails.route('conditions'));
+    }
+  }
+};
---
+++ api/schemas/relationship.schema.js
@@ -0,0 +1,51 @@
+module.exports = {
+    numYearsWasPatient: {
+        presence: {
+            allowEmpty: false,
+            message: '^Must pick an option'
+        }
+    },
+    visitNumber: {
+        presence: {
+            allowEmpty: false,
+            message: '^You must enter how many times the patient has visited your office in the past 12 months.'
+        },
+        numericality: true,
+    },
+    lastVisitDate: function (value) {
+      if (value) {
+        return {
+          validateDateFormat: {
+            message: '^Date of last office visit is not formatted correctly'
+          },
+          validateDateExists: {
+            message: '^Date of last office visit is not a valid date'
+          }
+        }
+      }
+      return {
+        presence: {
+          allowEmpty: false,
+          message: '^Date of last office visit is required'
+        },
+      }
+    },
+    firstTreatmentDate: function (value) {
+      if (value) {
+        return {
+          validateDateFormat: {
+            message: '^Date of first treatment is not formatted correctly'
+          },
+          validateDateExists: {
+            message: '^Date of first treatment is not a valid date'
+          }
+        }
+      }
+      return {
+        presence: {
+          allowEmpty: false,
+          message: '^Date of first treatment is required'
+        },
+      }
+    }
+}
