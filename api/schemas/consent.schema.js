974d8b21cc07da23ab337efca4d011fb106bdfbd
Dave Samojlenko <dsamojlenko@gmail.com>
2019-12-27 11:52:26 -0500

    add consent section

---
+++ api/schemas/consent.schema.js
@@ -0,0 +1,19 @@
+module.exports = {
+  consent: {
+    presence: {
+      message: '^You must select an option',
+      allowEmpty: false,
+    },
+  },
+  signature: function (value, attributes) {
+    const consent = attributes.consent;
+    if (consent === '1') {
+      return {
+        presence: {
+          message: '^Signature is Required if you Consent',
+          allowEmpty: false
+        }
+      }
+    }
+  },
+}
