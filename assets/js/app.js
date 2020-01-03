819b1f786119f1fbf42332dfb0352f4a6b1632ed
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-01-03 17:16:33 -0500

    configure and make validate and vueI18n available to vue

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,34 @@
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
 
 /**
- * Automatically register your Vue components.  Recursively scan this directory for the Vue
+ * Load validate.js on the window to make it availble to Vue
+ * components so we can re-use our existing schemas.
+ * Open to doing this in a different way.
+ */
+window.validate = require('validate.js');
+
+/**
+ * Load VueI18n library and our existing Sails locale files.
+ */
+Vue.use(VueI18n);
+
+const messages = {
+  en: require('../../config/locales/en.json'),
+  fr: require('../../config/locales/fr.json')
+};
+
+const i18n = new VueI18n({
+  locale: 'en',
+  messages,
+});
+
+/**
+ * Automatically register our Vue components.  Recursively scan this directory for the Vue
  * components and automatically register them with their "basename".
  *
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
-
 const files = require.context('./', true, /\.vue$/i);
 files.keys().map(key =>
   Vue.component(
@@ -19,7 +41,5 @@ files.keys().map(key =>
 );
 
 if (document.getElementById('app')) {
-  new Vue({
-    el: '#app',
-  });
+  new Vue({ i18n, el: '#app' });
 }
