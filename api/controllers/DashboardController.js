4a1b3ed6f5bcad0b7a7f61903356fbec132cacf3
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-01-03 16:32:28 -0500

    making page dynamic

--- api/controllers/DashboardController.js
+++ api/controllers/DashboardController.js
@@ -4,9 +4,33 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
+function ableToSubmit (sections) {
+  return sections.personal &&
+         sections.functional &&
+         sections.conditions &&
+         sections.medications &&
+         sections.treatments &&
+         sections.futureWork &&
+         sections.supportingDocs &&
+         sections.declaration;
+}
 
 module.exports = {
   index: function(req, res) {
-    res.view('pages/dashboard');
+    const sectionsCompleted = {
+      personal: true,
+      functional: true,
+      conditions: false,
+      medications: true,
+      treatments: true,
+      futureWork: false,
+      supportingDocs: true,
+      declaration: false
+    };
+
+    res.view('pages/dashboard', {
+      sectionsCompleted: sectionsCompleted,
+      ableToSubmit: ableToSubmit(sectionsCompleted)
+    });
   }
 };
