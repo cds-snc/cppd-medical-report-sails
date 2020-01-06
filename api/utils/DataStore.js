7aa18eaeb38414fa430586f4373991390cc565d9
Jeff Maher <jeff.maher@cds-snc.ca>
2020-01-06 10:21:36 -0500

    Fixed linting issues in DataStore.js

--- api/utils/DataStore.js
+++ api/utils/DataStore.js
@@ -3,43 +3,43 @@ const crypto = require('crypto');
 
 // --- Helper Functions/Variables ---
 const getDataFilePath = (applicationCode) => {
-    return 'sessions/' + applicationCode + '.json'; 
+  return 'sessions/' + applicationCode + '.json';
 };
 
 // --- Exported Functions ---
 const generateApplicationCode = () => {
-    // TODO Replace file system storage in favor of a database
-    
-    // Find a unique code
-    let code = null;
-    let fileName = null;
-    do {
-        code = crypto.randomBytes(3).toString('hex');
-        fileName = getDataFilePath(code);
-    } while(fs.existsSync(fileName));
-    
-    // Create the file
-    fs.openSync(fileName, 'w');
+  // TODO Replace file system storage in favor of a database
 
-    return code;
+  // Find a unique code
+  let code = null;
+  let fileName = null;
+  do {
+    code = crypto.randomBytes(3).toString('hex');
+    fileName = getDataFilePath(code);
+  } while(fs.existsSync(fileName));
+
+  // Create the file
+  fs.openSync(fileName, 'w');
+
+  return code;
 };
 
-const getMedicalReport = (applicationCode, birthDate) => {
-    // TODO Use the birthdate to verify a match of the code with the person
-    // TODO Replace file system storage in favor of a database
-    return JSON.parse(fs.readFileSync(getDataFilePath(applicationCode)));
-};
+// const getMedicalReport = (applicationCode, birthDate) => {
+//   // TODO Use the birthdate to verify a match of the code with the person
+//   // TODO Replace file system storage in favor of a database
+//   return JSON.parse(fs.readFileSync(getDataFilePath(applicationCode)));
+// };
 
 const storeMedicalReport = (medicalReportData) => {
-    // TODO Replace file system storage in favor of a database
-    fs.writeFileSync(
-        getDataFilePath(medicalReportData.applicationCode), 
-        JSON.stringify(medicalReportData, null, "\t")
-    );
+  // TODO Replace file system storage in favor of a database
+  fs.writeFileSync(
+        getDataFilePath(medicalReportData.applicationCode),
+        JSON.stringify(medicalReportData, null, '\t')
+  );
 };
 
 module.exports = {
-    generateApplicationCode,
-    getMedicalReport, 
-    storeMedicalReport
+  generateApplicationCode,
+  getMedicalReport,
+  storeMedicalReport
 };
