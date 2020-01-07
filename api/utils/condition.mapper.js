ba544472ae1fc3b8a5edc58d67c49e9e6c22c8ec
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-01-07 16:24:33 -0500

    name of condition field name changed

--- api/utils/condition.mapper.js
+++ api/utils/condition.mapper.js
@@ -23,7 +23,7 @@ const conditionReducer = (conditions) => {
   return _.reduce(
     conditions,
     (outList, condition, index) => {
-      outList[index] = condition.name_of_condition;
+      outList[index] = condition.conditionName;
       return outList;
     },
     {},
@@ -32,4 +32,4 @@ const conditionReducer = (conditions) => {
 
 const oneAttribute = (obj) => (Object.keys(obj).length === 1);
 
-module.exports =  { conditionReducer, oneAttribute };
+module.exports = { conditionReducer, oneAttribute };
