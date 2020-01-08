826f68c2a1bcb1aed5dd509cd30afb64b90dbd97
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-01-08 16:45:10 -0500

    New conditions flow (#57)
    * more explicitly test for conditions or empty conditions
    
    * new flow for conditions
    
    * validate on edit
    
    * change of direction
    
    * add required validation for ICD code
    
    * add some labels
    
    * bring back the docs uploader
    
    * add new string
    
    * conditional redirect
    
    * update button labels
    
    * make buttons match add_first
    
    * add all conditions message to subsequent adds

--- api/controllers/AddConditionController.js
+++ api/controllers/AddConditionController.js
@@ -9,14 +9,26 @@ const dataStore = require('../utils/DataStore');
 
 module.exports = {
   create: function (req, res) {
-    res.view('pages/conditions/add', {
+    if (_.has(req.session.medicalReport, 'conditions') && req.session.medicalReport.conditions.length) {
+      return res.view('pages/conditions/add', {
+        data: req.session.medicalReport
+      });
+    }
+
+    res.view('pages/conditions/add_first', {
       data: req.session.medicalReport
     });
   },
 
   store: function (req, res) {
     const body = Object.assign({}, req.body);
+
+    // use the value of the submit button to determine redirect
+    const action = body.save_and;
+
+    // delete stuff we don't want to save
     delete body._csrf;
+    delete body.save_and;
 
     let valid = req.validate(req, res, require('../schemas/condition.schema'));
 
@@ -27,8 +39,12 @@ module.exports = {
       }
       req.session.medicalReport.conditions.push(body);
       dataStore.storeMedicalReport(req.session.medicalReport);
+
+      if (action === 'add_another') {
+        return res.redirect(sails.route('conditions.add'));
+      }
+
       res.redirect(sails.route('conditions'));
     }
   }
 };
-
