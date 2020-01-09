746fdef3985fea9a260d250bbb3d7c6869d5f137
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2020-01-09 12:07:18 -0500

    Enable resuming a saved application (#61)
    * Added view for invitation screen
    
    * Corrected routes
    
    * Added page validation
    
    * Fixed validation message
    
    * Refined basic validation for invite screen
    
    * Added check w/o error message for code+birthdate
    
    * Detects and routes error, doesn't display yet
    
    * Renamed view
    
    * Remove debug dump on errors
    
    * Remove debug statement
    
    * Expose error message to user
    
    * Fixed linting error
    
    * Fixed linting warnings
    
    * Fixes LGTM assignment issue
    
    * Update en.json
    
    * refactor to fix multiple headers error
    
    Co-authored-by: Dave Samojlenko <dsamojlenko@gmail.com>

---
+++ api/controllers/MedicalProfessionalController.js
@@ -0,0 +1,40 @@
+/**
+ * MedicalProfessionalController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+const { applicationExists, getApplication } = require('../utils/DataStore');
+
+module.exports = {
+  index: function (req, res) {
+    /**
+     * If there is a medical report in the session, clear it
+     */
+    if (req.session.medicalReport) {
+      req.session.medicalReport = null;
+    }
+    res.view('pages/medical-professional');
+  },
+
+  store: function (req, res) {
+    // Validate completion / formatting, this calls res.redirect if invalid
+    let valid = req.validate(req, res, require('../schemas/invitation.schema'));
+
+    if (valid) {
+      // Validate whether or not the application code exists and, if so, matches the birthdate
+      const applicationCode = req.body.applicationCode;
+      let ok = applicationExists(applicationCode, req.body.birthdate);
+
+      if (ok) {
+        req.session.medicalReport = getApplication(applicationCode);
+        return res.redirect(sails.route('relationship'));
+      }
+
+      req.flash('error', 'errors.no_application_found');
+      req.flash('data', req.body);
+      return res.redirect('back');
+    }
+  }
+};
---
+++ api/schemas/invitation.schema.js
@@ -0,0 +1,21 @@
+const { dateValidators } = require('../hooks/validate/validators/dates.validator');
+
+module.exports = {
+  applicationCode: (value) => {
+    if (!value) {
+      return {
+        presence: {
+          allowEmpty: false,
+          message: '^errors.application_code_presence'
+        }
+      };
+    }
+    return {
+      format: {
+        pattern: /^[0-9a-zA-Z]{6}$/,
+        message: '^errors.application_code_format'
+      }
+    };
+  },
+  birthdate: (value) => dateValidators(value, '^errors.patient_birthdate_not_valid', '^errors.patient_birthdate_not_valid', '^errors.patient_birthdate_is_required'),
+};
