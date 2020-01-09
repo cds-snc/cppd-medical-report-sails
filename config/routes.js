5edd08932887361d1290e32877c200bd9ce6d76a
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-01-09 16:22:36 -0500

    Breaking medical professional into it's own file

--- config/routes.js
+++ config/routes.js
@@ -37,49 +37,9 @@ module.exports.routes = {
       fr: '/fr/debut'
     }
   },
-  'GET /en/medical-professional': {
-    name: 'medical-professional',
-    controller: 'MedicalProfessionalController',
-    action: 'index',
-    lang: 'en',
-    i18n: {
-      en: '/en/medical-professional',
-      fr: '/fr/medical-professional'
-    }
-  },
-  'POST /en/medical-professional': {
-    name: 'medical-professional.store',
-    controller: 'MedicalProfessionalController',
-    action: 'store',
-    lang: 'en',
-    i18n: {
-      en: '/en/medical-professional',
-      fr: '/fr/medical-professional'
-    }
-  },
-  'GET /en/doctor': { // convenience duplicate of medical-profession
-    name: 'doctor',
-    controller: 'MedicalProfessionalController',
-    action: 'index',
-    lang: 'en',
-    i18n: {
-      en: '/en/doctor',
-      fr: '/fr/doctor'
-    }
-  },
-  'POST /en/doctor': { // convenience duplicate of medical-profession
-    name: 'doctor.store',
-    controller: 'MedicalProfessionalController',
-    action: 'store',
-    lang: 'en',
-    i18n: {
-      en: '/en/doctor',
-      fr: '/fr/doctor'
-    }
-  },
 
+  ... require('./routes/medical-professional'),
   ... require('./routes/dashboard'),
-
   ... require('./routes/conditions'),
   ... require('./routes/confirmation'),
   ... require('./routes/consent'),
