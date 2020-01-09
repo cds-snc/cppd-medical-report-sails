a6d99028ac8b8323216c4162a96048e82d55dcae
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-01-09 14:03:35 -0500

    add controller, schema, view

---
+++ api/controllers/FutureWorkCapacityController.js
@@ -0,0 +1,39 @@
+/**
+ * FutureWorkCapacityController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  index: function (req, res) {
+    let data = req.session.medicalReport;
+
+    /**
+     * If we're returning to the form with flash data in locals,
+     * merge it with the rest of the medicalReport in the session.
+     */
+    if (res.locals.data) {
+      data = _.merge(res.locals.data, req.session.medicalReport);
+    }
+
+    res.view('pages/work', {
+      data: data
+    });
+  },
+
+  store: function (req, res) {
+    let valid = req.validate(req, res, require('../schemas/work.schema'));
+
+    if (valid) {
+      // save the model
+      req.session.medicalReport.returnToWork = req.body.returnToWork;
+      req.session.medicalReport.returnToWorkWhen = req.body.returnToWorkWhen;
+      req.session.medicalReport.typeOfWork = req.body.typeOfWork;
+      req.session.medicalReport.workDetails = req.body.workDetails;
+      dataStore.storeMedicalReport(req.session.medicalReport);
+
+      res.redirect(sails.route('dashboard'));
+    }
+  }
+};
