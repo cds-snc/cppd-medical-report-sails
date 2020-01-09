028666395d399bf9e3ed13ab7de498b99861f98d
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-01-09 14:22:26 -0500

    add validation rules

--- api/schemas/work.schema.js
+++ api/schemas/work.schema.js
@@ -1,3 +1,28 @@
 module.exports = {
-
+  returnToWork: {
+    presence: {
+      allowEmpty: false,
+      message: '^Return to work is required'
+    }
+  },
+  returnToWorkWhen: function (value, attributes) {
+    if (attributes.returnToWork && attributes.returnToWork === '1') {
+      return {
+        presence: {
+          allowEmpty: false,
+          message: '^Required if return to work is yes'
+        }
+      }
+    }
+  },
+  typeOfWork: function (value, attributes) {
+    if (attributes.returnToWork && attributes.returnToWork === '1') {
+      return {
+        presence: {
+          allowEmpty: false,
+          message: '^Required if return to work is yes'
+        }
+      }
+    }
+  }
 }
