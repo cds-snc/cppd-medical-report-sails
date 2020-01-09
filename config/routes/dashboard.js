0d4b43172173184bd55e0ab703e552b9deaf4344
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-01-09 13:11:03 -0500

    Breaking the route into it's own file

---
+++ config/routes/dashboard.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'GET /en/dashboard': {
+    name: 'dashboard',
+    controller: 'DashboardController',
+    action: 'index',
+    lang: 'en',
+    i18n: {
+      en: '/en/dashboard',
+      fr: '/fr/tabeleaudebord'
+    }
+  }
+};
