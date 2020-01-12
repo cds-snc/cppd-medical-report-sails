23b96b23451e8c577d20ab3866347397f0ecee2a
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-01-11 21:54:06 -0500

    add medications question

--- config/routes.js
+++ config/routes.js
@@ -185,6 +185,17 @@ module.exports.routes = {
     }
   },
 
+  'POST /en/medications': {
+    name: 'medications.save',
+    controller: 'MedicationsController',
+    action: 'save',
+    lang: 'en',
+    i18n: {
+      en: '/en/medications',
+      fr: '/fr/medications'
+    }
+  },
+
   'GET /en/medications/add': {
     name: 'medications.add',
     controller: 'AddMedicationController',
