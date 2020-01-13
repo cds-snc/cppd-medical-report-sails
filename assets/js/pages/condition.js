48d571afb058b74223795a9d8261a2a8888c4c78
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2020-01-13 10:33:41 -0500

    Conditions: Explain why and provide more questions always appear, not required (#92)

--- assets/js/pages/condition.js
+++
@@ -1,4 +0,0 @@
-const checkUnknowns = require('./checkUnknowns');
-
-checkUnknowns('unknown_outlook_details', 'conditionOutlook', '4');
-checkUnknowns('unknown_symptoms_occur_details', 'symptomsOccur', '3');
