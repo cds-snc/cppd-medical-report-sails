973c8cdb5454cb9abc1401885e9dc5b8a7ef6390
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-01-16 12:34:58 -0500

    save condition to db

--- api/controllers/AddConditionController.js
+++ api/controllers/AddConditionController.js
@@ -5,18 +5,23 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
-const dataStore = require('../utils/DataStore');
-const Documents = require('../utils/DocumentsHelper');
-
 module.exports = {
-  create: function (req, res) {
+  create: async function (req, res) {
+    // load the medical report
+    let medicalReport = await MedicalReport.findOne({
+      where: {
+        applicationCode: req.session.applicationCode
+      }
+    });
+    
+    // TODO: Will circle back to handle the Documents later
     if (!res.locals.data) {
       res.locals.data = {
         conditionFiles: ''
       }
     }
 
-    if (_.has(req.session.medicalReport, 'conditions') && req.session.medicalReport.conditions.length) {
+    if (medicalReport.Conditions) {
       return res.view('pages/conditions/add', {
         medicalReport: req.session.medicalReport
       });
@@ -27,27 +32,34 @@ module.exports = {
     });
   },
 
-  store: function (req, res) {
-    const body = Object.assign({}, req.body);
+  store: async function (req, res) {
+    // load the medical report
+    let medicalReport = await MedicalReport.findOne({
+      where: {
+        applicationCode: req.session.applicationCode
+      }
+    });
 
     // use the value of the submit button to determine redirect
-    const action = body.save_and;
-
-    // delete stuff we don't want to save
-    delete body._csrf;
-    delete body.save_and;
+    const action = req.body.save_and;
 
     let valid = req.validate(req, res, require('../schemas/condition.schema'));
 
     if (valid) {
       // save model here
-      if (!_.has(req.session.medicalReport, 'conditions')) {
-        req.session.medicalReport.conditions = [];
-      }
-      req.session.medicalReport.conditions.push(body);
+      Condition.create({
+        MedicalReportId: medicalReport.id,
+        icdCode: req.body.icdCode,
+        symptomsBegan: req.body.symptomsBegan,
+        clinicallyImpair: req.body.clinicallyImpair,
+        conditionOutlook: req.body.conditionOutlook,
+        conditionOutlookUnknown: req.body.conditionOutlookUnknown,
+        conditionLast: req.body.conditionLast,
+        symptomsOccur: req.body.symptomsOccur,
+        symptomsOccurUnknown: req.body.symptomsOccurUnknown
+      });
 
-      Documents.saveDocumentsFromCondition(req.session.medicalReport, body);
-      dataStore.storeMedicalReport(req.session.medicalReport);
+      // Documents.saveDocumentsFromCondition(req.session.medicalReport, body);
 
       if (action === 'add_another') {
         return res.redirect(sails.route('conditions.add'));
