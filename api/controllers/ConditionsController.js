9ae1f5a8a90be2c353895d7de307a19d87824527
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-01-16 12:35:26 -0500

    get conditions from db, and super-simplify the controller

--- api/controllers/ConditionsController.js
+++ api/controllers/ConditionsController.js
@@ -5,41 +5,21 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
-const validate = require('../hooks/validate/validator');
-const conditionsSchema = require('../schemas/condition.schema');
-
-/**
- * Will return an array of indicies identifying incomplete conditions.
- * 
- * For example, if `conditions` as [condA, condB, condC] and A and C are incomplete,
- * it'll return [1,3]. 
- * 
- * Indicies start at 1, rather than zero, which lines up with the current way
- * the conditions/index view works.
- * 
- * @param {*} conditions Array of conditions
- */
-function getIncompleteConditions(conditions) {
-  incompleteConditions = []
-  for (let i = 0; i < conditions.length; i++) {
-    if ( validate(conditions[i], conditionsSchema) !== undefined ) {
-      incompleteConditions.push(i+1);
-    }
-  }
-  return incompleteConditions;
-}
-
 module.exports = {
-  index: function (req, res) {
-    let medicalReport = req.session.medicalReport;
-    if (_.has(medicalReport, 'conditions') && medicalReport.conditions.length) {
-      let incompleteConditions = getIncompleteConditions(medicalReport.conditions);
-      return res.view('pages/conditions/index', {
-        data: req.session.medicalReport,
-        incompleteConditions: incompleteConditions,
-      });
+  index: async function (req, res) {
+    let medicalReport = await MedicalReport.findOne({
+      where: {
+        applicationCode: req.session.applicationCode
+      },
+      include: [
+        {model: Condition, as: 'Conditions'}
+      ]
+    });
+
+    if (!medicalReport.Conditions.length) {
+      return res.redirect(sails.route('conditions.add'));
     }
 
-    return res.redirect(sails.route('conditions.add'));
+    return res.view('pages/conditions/index', { data: medicalReport });
   }
 };
