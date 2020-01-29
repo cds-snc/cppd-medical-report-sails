2f20644c923dc1a86313575cc90ce9e403011fff
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-01-29 17:49:04 -0500

    simpler method

--- api/utils/ApplicationCodeHelper.js
+++ api/utils/ApplicationCodeHelper.js
@@ -3,16 +3,15 @@ const crypto = require('crypto');
 const generate = async () => {
   let code = null;
 
-  let existingCodes = await MedicalReport.findAll({
-    attributes: ['applicationCode'],
-    raw: true
-  });
-
-  const codes = _.pluck(existingCodes, 'applicationCode');
-
   do {
     code = crypto.randomBytes(3).toString('hex').toUpperCase();
-  } while (codes.indexOf(code) !== -1);
+  } while (
+    await MedicalReport.findOne({
+      where: {
+        applicationCode: code
+      }
+    }) !== null
+  );
 
   return code;
 }
