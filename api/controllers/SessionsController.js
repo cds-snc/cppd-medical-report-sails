aa983599ed8ff50477e4a415c49e2752f2018d87
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-01-30 15:46:29 -0500

    remove unused import

--- api/controllers/SessionsController.js
+++ api/controllers/SessionsController.js
@@ -5,14 +5,12 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
-const conditionHelper = require('../utils/ConditionHelpers');
-
 module.exports = {
   index: async function (req, res) {
 
-    const reports = await MedicalReport.findAll({limit: 10, attributes: ['id', 'firstName', 'lastName', 'socialInsuranceNumber']});
-    sails.log.silly(`Query returned: ${JSON.stringify(reports, null,  2)}`);
-    const viewModel = reports.map(x =>  {
+    const reports = await MedicalReport.findAll({ limit: 10, attributes: ['id', 'firstName', 'lastName', 'socialInsuranceNumber'] });
+    sails.log.silly(`Query returned: ${JSON.stringify(reports, null, 2)}`);
+    const viewModel = reports.map(x => {
       return {
         id: x.id,
         dateReceived: 'Not Implemented',
@@ -34,17 +32,17 @@ module.exports = {
     const reportId = req.params.session;
     sails.log.silly(`Requesing medical report: ${reportId}`);
     const medicalReport = await MedicalReport.findOne({
-      where: {id: reportId},
+      where: { id: reportId },
       include: [{
         model: Condition,
         as: 'Conditions',
-        include: [ {
+        include: [{
           model: Treatment,
           as: 'Treatments'
         }, {
           model: Medication,
           as: 'Medications'
-        },{
+        }, {
           model: Document,
           as: 'Documents'
         }]
@@ -54,7 +52,7 @@ module.exports = {
     sails.log.silly(`medicalReport: ${JSON.stringify(medicalReport.toJSON(), null, 2)}`);
 
     res.view('pages/sessions/view', {
-      data:medicalReport,
+      data: medicalReport,
       symptomsOccur: require('../utils/support/symptomsOccur'),
       conditionOutlook: require('../utils/support/conditionOutlook'),
       stopWorking: require('../utils/support/stopWorking'),
