d6b6dfb4e89550f34d88af3515cdfce9b9d50554
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-01-30 16:28:55 -0500

    removed unused route and controller method

--- api/controllers/ConfirmationController.js
+++ api/controllers/ConfirmationController.js
@@ -5,17 +5,8 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
-const dataStore = require('../utils/DataStore');
-
 module.exports = {
   index: function (req, res) {
     res.view('pages/confirmation');
   },
-
-  store: function (req, res) {
-    dataStore.storeMedicalReport(req.session.medicalReport);
-    req.session.medicalReport = {};
-
-    res.redirect(sails.route('start'));
-  }
 };
