616a2979d8394ca0ced56538939d8ee6805c46fc
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-01-31 16:34:29 -0500

    handle uploading files, as well as deleting physical files

--- assets/js/components/FileUpload.vue
+++ assets/js/components/FileUpload.vue
@@ -6,7 +6,7 @@
         class="w-64 border-2 border-black cursor-pointer bg-gray-200 px-5 py-2 inline-block text-center"
       >
         <span>{{ this.uploadLabel }}</span>
-        <input type="file" ref="file" @change="onSelect" class="hidden" />
+        <input type="file" ref="file" @change="handleFileUpload" class="hidden" />
       </label>
     </div>
     <div class="mt-4">
@@ -55,33 +55,56 @@ export default {
     }
   },
   methods: {
-    onSelect() {
+    handleFileUpload() {
       const file = this.$refs.file.files[0];
-      this.addFile(file.name);
+      this.addFile(file);
     },
     addFile(file) {
+      let formData = new FormData();
+      formData.append("file", file);
+
+      // TODO: validate file is a file
+
       axios
-        .post("/api/documents", {
-          file: file
-        })
-        .then(response => {
-          this.uploaded_files.push({
-            id: response.data.id,
-            fileName: response.data.originalFileName
-          });
-        });
-    },
-    removeFile(file) {
-      // if we're editing vs creating
-      axios
-        .delete("/api/conditions/" + this.conditionId + "/documents", {
-          data: {
-            docId: file.id
+        .post("/api/documents", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data"
           }
         })
         .then(response => {
-          this.uploaded_files.splice(this.uploaded_files.indexOf(file), 1);
+          this.uploaded_files.push({
+            id: response.data.document.id,
+            fileName: response.data.document.originalFileName
+          });
+        })
+        .catch(error => {
+          console.log(error);
         });
+    },
+    removeFile(file) {
+      if (this.conditionId) {
+        // on edit, we need to detach from the current condition
+        axios
+          .delete("/api/conditions/" + this.conditionId + "/documents", {
+            data: {
+              docId: file.id
+            }
+          })
+          .then(response => {
+            this.uploaded_files.splice(this.uploaded_files.indexOf(file), 1);
+          });
+      } else {
+        // on create, there is no Condition
+        axios
+          .delete("/api/documents", {
+            data: {
+              id: file.id
+            }
+          })
+          .then(response => {
+            this.uploaded_files.splice(this.uploaded_files.indexOf(file), 1);
+          });
+      }
     },
     getFiles(conditionId) {
       axios
