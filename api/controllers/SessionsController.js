30d009344f2fce1c354e8f643ac650ccdb4b0dba
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-01-31 08:37:26 -0500

    Medical Adjudicator Session View and Report View for Database Integration Branch (#115)
    * Working on ma-view
    
    * Working on sessions page
    
    * Fixing semmle error
    
    * Removing unused download functionality
    
    * Removing unused variable
    
    * Using Nunjucks cycler
    
    * Resolving styling (css and code)
    
    * Doing a single query
    
    * Adding conditions and treatments
    
    * remove unused import
    
    Co-authored-by: Dave Samojlenko <dsamojlenko@gmail.com>

--- api/controllers/SessionsController.js
+++ api/controllers/SessionsController.js
@@ -5,59 +5,59 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
-const fs = require('fs');
-const path = require('path');
-const conditionHelper = require('../utils/ConditionHelpers');
-
 module.exports = {
-  index: function (req, res) {
+  index: async function (req, res) {
 
-    let directory = path.resolve(__dirname, '../../sessions');
+    const reports = await MedicalReport.findAll({ limit: 10, attributes: ['id', 'firstName', 'lastName', 'socialInsuranceNumber'] });
+    sails.log.silly(`Query returned: ${JSON.stringify(reports, null, 2)}`);
+    const viewModel = reports.map(x => {
+      return {
+        id: x.id,
+        dateReceived: 'Not Implemented',
+        applicant: `${x.lastName}, ${x.firstName}`,
+        sin: x.socialInsuranceNumber,
+        lastViewed: 'Not Implemented'
+      };
+    });
 
-    fs.readdir(directory, (err, files) => {
-      if (!err) {
-        _.pull(files, '.gitignore');
-        res.view('pages/sessions', {
-          files: files
-        });
-      }
+    const totalReports = await MedicalReport.count();
+    sails.log.silly(`totalReports: ${totalReports}`);
+    return res.view('pages/sessions', {
+      reports: viewModel,
+      total: totalReports
     });
   },
 
-  download: function (req, res) {
-    const filename = req.params.session;
-    const sessionFile = path.resolve(__dirname, '../../sessions/' + filename);
+  view: async function (req, res) {
+    const reportId = req.params.session;
+    sails.log.silly(`Requesing medical report: ${reportId}`);
+    const medicalReport = await MedicalReport.findOne({
+      where: { id: reportId },
+      include: [{
+        model: Condition,
+        as: 'Conditions',
+        include: [{
+          model: Treatment,
+          as: 'Treatments'
+        }, {
+          model: Medication,
+          as: 'Medications'
+        }, {
+          model: Document,
+          as: 'Documents'
+        }]
 
-    var SkipperDisk = require('skipper-disk');
-    var fileAdapter = SkipperDisk(/* optional opts */);
+      }]
+    });
+    sails.log.silly(`medicalReport: ${JSON.stringify(medicalReport.toJSON(), null, 2)}`);
 
-    // set the filename to the same file as the user uploaded
-    res.set('Content-disposition', 'attachment; filename=' + filename);
-
-    // Stream the file down
-    fileAdapter.read(sessionFile)
-      .on('error', (err) => {
-        return res.serverError(err);
-      })
-      .pipe(res);
-  },
-
-  view: function (req, res) {
-    const filename = req.params.session;
-    const sessionFile = path.resolve(__dirname, '../../sessions/' + filename);
-
-    const medicalReport = JSON.parse(fs.readFileSync(sessionFile));
-    const conditions = conditionHelper.getConditionsWithMedicationsAndTreatments(medicalReport);
-    // console.log(require('../utils/support/symptomsOccur')[1]);
     res.view('pages/sessions/view', {
       data: medicalReport,
-      conditions: conditions,
       symptomsOccur: require('../utils/support/symptomsOccur'),
       conditionOutlook: require('../utils/support/conditionOutlook'),
       stopWorking: require('../utils/support/stopWorking'),
       returnToWorkWhen: require('../utils/support/returnToWorkWhen'),
       typeOfWork: require('../utils/support/typeOfWork'),
-      getDocumentsByCondition: require('../utils/DocumentsHelper').getDocumentsByCondition,
       moment: require('moment')
     });
   }
