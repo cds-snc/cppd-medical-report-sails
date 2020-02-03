bf4c0eedbc4e12d6c3cf32398287b90f7edf3102
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-02-03 15:43:06 -0500

    add practitioner section

---
+++ config/routes/practitioner.js
@@ -0,0 +1,26 @@
+module.exports.routes = {
+  /**
+   * Practitioner ROUTES
+   */
+  'GET /en/practitioner': {
+    name: 'practitioner',
+    controller: 'PractitionerController',
+    action: 'index',
+    lang: 'en',
+    i18n: {
+      en: '/en/practitioner',
+      fr: '/fr/practitioner'
+    }
+  },
+
+  'POST /en/practitioner': {
+    name: 'practitioner.store',
+    controller: 'PractitionerController',
+    action: 'store',
+    lang: 'en',
+    i18n: {
+      en: '/en/practitioner',
+      fr: '/fr/practitioner'
+    }
+  },
+}
