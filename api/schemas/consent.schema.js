55bee77a3476053d97bc84e125608d2a81a0f5b2
Jeff Maher <jeff.maher@cds-snc.ca>
2020-02-04 09:30:22 -0500

    Added validation messages

--- api/schemas/consent.schema.js
+++ api/schemas/consent.schema.js
@@ -5,6 +5,34 @@ module.exports = {
       allowEmpty: false,
     },
   },
+  signature_mode: {
+    presence: {
+      message: '^You must select an option',
+      allowEmpty: false,
+    },
+  },
+  signature_draw_data: function (value, attributes) {
+    const mode = attributes.signature_mode;
+    if (mode === 'draw') {
+      return {
+        presence: {
+          message: '^Signature is required',
+          allowEmpty: false
+        }
+      };
+    }
+  },
+  signature_typed: function (value, attributes) {
+    const mode = attributes.signature_mode;
+    if (mode === 'type') {
+      return {
+        presence: {
+          message: '^Signature is required',
+          allowEmpty: false
+        }
+      };
+    }
+  },
   signature: function (value, attributes) {
     const consent = attributes.consent;
     if (consent === '1') {
