20e291450e9bf0efe49c22d7fa234c5c7559cf61
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-02-12 21:56:35 -0500

    add axe a11y scanning for cypress

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+require('./commands');
+require('cypress-axe');
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
