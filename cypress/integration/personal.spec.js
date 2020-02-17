f235008503e7ead8b505885e5ccd946da84aa7cd
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-02-17 13:42:35 -0500

    test email and date validations

--- cypress/integration/personal.spec.js
+++ cypress/integration/personal.spec.js
@@ -63,6 +63,130 @@ describe('Test validation on Personal form', () => {
     cy.injectAxe().checkA11y();
   });
 
-  // email validation
-  // date validation
+  describe('Test date validation', () => {
+    it('fails February 31st', () => {
+      cy.get('[name=birthdateDay]').type('31');
+      cy.get('[name=birthdateMonth]').type('2');
+      cy.get('[name=birthdateYear]').type('1999');
+      cy.get('[type="submit"]').click();
+
+      cy.get('#content .error-list').contains('Birthdate is not a valid date');
+      cy.get('[name=birthdateMonth]').should('have.attr', 'aria-invalid', 'true');
+      cy.get('[name=birthdateDay]').should('have.attr', 'aria-invalid', 'true');
+      cy.get('[name=birthdateYear]').should('have.attr', 'aria-invalid', 'true');
+    });
+
+    it('fails February 29th in a non-leap year', () => {
+      cy.visit('/en/personal');
+      cy.get('[name=birthdateDay]').type('29');
+      cy.get('[name=birthdateMonth]').type('2');
+      cy.get('[name=birthdateYear]').type('1999');
+      cy.get('[type="submit"]').click();
+
+      cy.get('#content .error-list').contains('Birthdate is not a valid date');
+      cy.get('[name=birthdateMonth]').should('have.attr', 'aria-invalid', 'true');
+      cy.get('[name=birthdateDay]').should('have.attr', 'aria-invalid', 'true');
+      cy.get('[name=birthdateYear]').should('have.attr', 'aria-invalid', 'true');
+    });
+
+    it('passes February 29th in a leap year', () => {
+      cy.visit('/en/personal');
+      cy.get('[name=birthdateDay]').type('29');
+      cy.get('[name=birthdateMonth]').type('2');
+      cy.get('[name=birthdateYear]').type('2000');
+      cy.get('[type="submit"]').click();
+
+      cy.get('#content .error-list').contains('Birthdate is not a valid date').should('not.exist');
+      cy.get('[name=birthdateMonth]').should('not.have.attr', 'aria-invalid');
+      cy.get('[name=birthdateDay]').should('not.have.attr', 'aria-invalid');
+      cy.get('[name=birthdateYear]').should('not.have.attr', 'aria-invalid');
+    });
+
+    it('fails April 31st', () => {
+      cy.visit('/en/personal');
+      cy.get('[name=birthdateDay]').type('31');
+      cy.get('[name=birthdateMonth]').type('4');
+      cy.get('[name=birthdateYear]').type('1999');
+      cy.get('[type="submit"]').click();
+
+      cy.get('#content .error-list').contains('Birthdate is not a valid date');
+      cy.get('[name=birthdateMonth]').should('have.attr', 'aria-invalid', 'true');
+      cy.get('[name=birthdateDay]').should('have.attr', 'aria-invalid', 'true');
+      cy.get('[name=birthdateYear]').should('have.attr', 'aria-invalid', 'true');
+    });
+
+    it('fails June 31st', () => {
+      cy.get('[name=birthdateDay]').type('31');
+      cy.get('[name=birthdateMonth]').type('6');
+      cy.get('[name=birthdateYear]').type('1999');
+      cy.get('[type="submit"]').click();
+
+      cy.get('#content .error-list').contains('Birthdate is not a valid date');
+      cy.get('[name=birthdateMonth]').should('have.attr', 'aria-invalid', 'true');
+      cy.get('[name=birthdateDay]').should('have.attr', 'aria-invalid', 'true');
+      cy.get('[name=birthdateYear]').should('have.attr', 'aria-invalid', 'true');
+    });
+
+    it('fails September 31st', () => {
+      cy.get('[name=birthdateDay]').type('31');
+      cy.get('[name=birthdateMonth]').type('9');
+      cy.get('[name=birthdateYear]').type('1999');
+      cy.get('[type="submit"]').click();
+
+      cy.get('#content .error-list').contains('Birthdate is not a valid date');
+      cy.get('[name=birthdateMonth]').should('have.attr', 'aria-invalid', 'true');
+      cy.get('[name=birthdateDay]').should('have.attr', 'aria-invalid', 'true');
+      cy.get('[name=birthdateYear]').should('have.attr', 'aria-invalid', 'true');
+    });
+
+    it('fails November 31st', () => {
+      cy.get('[name=birthdateDay]').type('31');
+      cy.get('[name=birthdateMonth]').type('11');
+      cy.get('[name=birthdateYear]').type('1999');
+      cy.get('[type="submit"]').click();
+
+      cy.get('#content .error-list').contains('Birthdate is not a valid date');
+      cy.get('[name=birthdateMonth]').should('have.attr', 'aria-invalid', 'true');
+      cy.get('[name=birthdateDay]').should('have.attr', 'aria-invalid', 'true');
+      cy.get('[name=birthdateYear]').should('have.attr', 'aria-invalid', 'true');
+    });
+  });
+
+  describe('Test email validation', () => {
+    it('Passes validation if no email is provided', () => {
+      cy.get('[type="submit"]').click();
+      cy.get('#content .error-list').contains('valid email').should('not.exist');
+    });
+
+    it('Fails validation on invalid format email', () => {
+      cy.get('[name=email]').type('asdf');
+      cy.get('[type="submit"]').click();
+      cy.get('#content .error-list').contains('valid email');
+    });
+
+    it('Fails validation on partial email', () => {
+      cy.get('[name=email]').type('asdf@');
+      cy.get('[type="submit"]').click();
+      cy.get('#content .error-list').contains('valid email');
+
+      cy.visit('/en/personal');
+      cy.get('[name=email]').type('asdf@asdf');
+      cy.get('[type="submit"]').click();
+      cy.get('#content .error-list').contains('valid email');
+    });
+
+    it('Passes validation on a normal email', () => {
+      cy.get('[name=email]').type('email@test.com');
+      cy.get('[type="submit"]').click();
+      cy.get('#content .error-list').contains('valid email').should('not.exist');
+
+      cy.visit('/en/personal');
+      cy.get('[name=email]').type('email+test@test.com');
+      cy.get('[type="submit"]').click();
+      cy.get('#content .error-list').contains('valid email').should('not.exist');
+    });
+  });
+
 });
+
+
