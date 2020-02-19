4f1fcc1e630066c8f4b982a58b4a0117ffd84549
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-02-19 17:36:31 -0500

    min config required to run in production mode without breaking

--- config/env/production.js
+++ config/env/production.js
@@ -250,10 +250,11 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      process.env.BASE_URL
+      //   'https://example.com',
+      //   'https://staging.example.com',
+    ],
 
 
     /***************************************************************************
