e0dd758350132b2e7678e6f9bbb60ca60774325f
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-02-21 15:46:34 -0500

    test for can't delete yourself

--- cypress/integration/user_management/delete_user.spec.js
+++ cypress/integration/user_management/delete_user.spec.js
@@ -1,6 +1,7 @@
 const faker = require('faker');
 
 describe('Test the Delete User flow', () => {
+  const adminUserName = 'Test Admin';
   const adminUser = 'admin@user.com';
   const adminPass = 'secret';
   const testUserName = faker.name.findName();
@@ -22,12 +23,24 @@ describe('Test the Delete User flow', () => {
     // find the user record we just created
     cy.get('[data-cy=name]').contains('td', testUserName)
       .parent()
-      .within($tr => {
+      .within(() => {
         cy.get('td a')
           .contains('Delete')
           .click();
       });
 
     cy.get('[data-cy=name]').contains('td', testUserName).should('not.exist');
+  });
+
+  it('cant delete yourself', () => {
+    cy.login(adminUser, adminPass);
+    cy.visit('/en/users');
+
+    cy.get('[data-cy=name]').contains('td', adminUserName)
+      .parent()
+      .within(() => {
+        cy.get('td a')
+          .contains('Delete').should('not.exist');
+      });
   });
 });
