b3e4caf1d675fef441dc962cef0339bc926a8032
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-02-26 16:05:11 -0500

    use dbseed command

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,10 +1,6 @@
 describe('Test the authentication flow for medical adjudicators', () => {
   before(() => {
-    /**
-     * TODO: When we add our migrations, this should
-     * reset the db then seed the test data.
-     */
-    cy.exec('npm run db:seed:undo && npm run db:seed');
+    cy.dbseed();
 
     cy.visit('/en/login');
   });
