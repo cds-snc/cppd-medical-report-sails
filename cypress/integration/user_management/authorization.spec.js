7f7ec4b85b10965a0be326197649b6edb3e10769
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-02-26 17:18:38 -0500

    User management (#157)
    * add passport and bcrypt packages
    
    * add User mode
    
    * quick command-line user creator
    
    * just checking stuff in
    
    * use proper redirect syntax
    
    * use proper instanceMethod syntax
    
    * delete some old code
    
    * checkpoint commit
    
    * add logout route
    
    * better policy config
    
    * add after-a11y check
    
    * add some auth tests
    
    * ignore test files
    
    * create test user
    
    * add option to reset seeds
    
    * remove unused assignment
    
    * remove unused import
    
    * rename test files
    
    * use isAuthenticated on session
    
    * ignore cypress artifacts
    
    * add login command
    
    * add some more tests
    
    * added some comments
    
    * added some comments
    
    * added some comments
    
    * revert some changes that belong in another pr
    
    * add feature flag for auth
    
    * better example
    
    * add FeatureFlags util
    
    * use FeatureFlags util
    
    * whitespace
    
    * switch to multi-stage build
    
    * install sails in final container
    
    * save some time with a production build
    
    * ok we're not ready for production build
    
    * removed because we're not using them
    
    * cleanup remaining defaultPolicies stuff
    
    * copy only what's needed in build step
    
    * oops
    
    * add field validation
    
    * use macros and support password input
    
    * just delete the test user on rollback
    
    * update to match design
    
    * Auth: Fixes Docker dev setup (#153)
    
    * changed the heading on the login screen
    
    * setup routes, controller, policy for admin users
    
    * start setting up tests for user mgmt
    
    * start building out create form and tests
    
    * speed things up a bit - just run the seed once
    
    * speed things up a bit, use cy.request instead of navigating the form
    
    * create user
    
    * use data-cy attributes for targeting
    
    * begin edit user and tests
    
    * use a title that makes sense
    
    * whitespace
    
    * remove password fields on edit
    
    * redirect if user doesn't exist
    
    * update cypress to 4.0.2
    
    * edit user functionality and tests
    
    * add delete action and confirm
    
    * delete user, confirmation, and tests
    
    * cleanup
    
    * remove reset password link for now
    
    * redirect to users if admin logs in
    
    * you can't delete yourself
    
    * don't check for location after login because it changes based on role
    
    * test for can't delete yourself
    
    * run cypress tests
    
    * not ready to run cypress tests yet
    
    * properly ignore videos/screenshots
    
    * bring inline with design
    
    * add logout link and tests
    
    * hide reset password for now
    
    * more design changes
    
    * fix label
    
    * get tests to passing (updated titles)
    
    Co-authored-by: Jeff Maher <950486+jeffmaher@users.noreply.github.com>

---
+++ cypress/integration/user_management/authorization.spec.js
@@ -0,0 +1,27 @@
+describe('Test the User Management authorization', () => {
+  before(() => {
+    /**
+     * TODO: When we add our migrations, this should
+     * reset the db then seed the test data.
+     */
+    cy.exec('npm run db:seed:undo && npm run db:seed');
+  });
+
+  it('only allows users with isAdmin flag to access users section', () => {
+    // unauthenticated
+    cy.visit('/en/users');
+    cy.get('h1').contains('Medical adjudication sign in');
+
+    // authenticated but not admin
+    cy.login('test@user.com', 'secret');
+    cy.visit('/en/users');
+    cy.get('h1').contains('Sessions');
+
+    cy.logout();
+
+    // authenticated and isAdmin
+    cy.login('admin@user.com', 'secret');
+    cy.visit('/en/users');
+    cy.get('h1').contains('Account management');
+  });
+});
