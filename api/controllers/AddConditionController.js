a93cd543fd4b224572cedd762cede95cfd6d75ea
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-02-27 17:54:02 -0500

    fix back button and condition add bug

--- api/controllers/AddConditionController.js
+++ api/controllers/AddConditionController.js
@@ -29,7 +29,7 @@ module.exports = {
       };
     }
 
-    if (medicalReport.Conditions) {
+    if (medicalReport.Conditions.length) {
       return res.view('pages/conditions/add', {
         medicalReport: medicalReport
       });
