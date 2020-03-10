5ba1b0dc6fca2e4d129c463d72c99fd40586bcfb
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-03-10 11:33:39 -0400

    add Basic Patient Information section validation tests

---
+++ cypress/integration/medical_report/basic_patient_information.spec.js
@@ -0,0 +1,88 @@
+const faker = require('faker');
+
+const firstName = faker.name.firstName();
+const lastName = faker.name.lastName();
+const name = firstName + ' ' + lastName;
+
+describe('Test the basic patient information section', () => {
+
+  beforeEach(() => {
+    cy.personal(firstName, lastName, 9, 9, 1999);
+    cy.consent(name);
+
+    cy.visit('/en/invite');
+
+    cy.get('[data-cy=applicationCode]').then(($code) => {
+      const code = $code.text();
+
+      cy.loginDoctor(code, '9', '9', '1999');
+
+      cy.visit('/en/dashboard');
+      cy.url().should('include', '/en/dashboard');
+      cy.get('h1').contains('Medical report for Canada Pension Plan Disability Benefits');
+      cy.get('h1').contains('Patient: ' + name);
+    });
+  });
+
+  it('validates required fields', () => {
+    cy.visit('/en/relationship');
+    cy.injectAxe().checkA11y();
+
+    cy.get('[type="submit"]').click();
+    cy.injectAxe().checkA11y();
+
+    cy.get('#content .error-list').contains('When did they come into your care is required');
+    cy.get('[name=relationshipStarted]').should('have.attr', 'aria-invalid', 'true');
+
+    cy.get('#content .error-list').contains('Date of first treatment is required');
+    cy.get('[name=firstTreatmentDate]').should('have.attr', 'aria-invalid', 'true');
+
+    cy.get('#content .error-list').contains('You must enter how many times the patient has visited your office in the past 12 months.');
+    cy.get('[name=visitNumber]').should('have.attr', 'aria-invalid', 'true');
+
+    cy.get('#content .error-list').contains('Date of last office visit is required');
+    cy.get('[name=lastVisitDate]').should('have.attr', 'aria-invalid', 'true');
+
+    cy.get('#content .error-list').contains('Stopped working is required');
+    cy.get('[name=stopWorking]').should('have.attr', 'aria-invalid', 'true');
+
+    cy.get('#content .error-list').contains('Stopped working is required');
+    cy.get('[name=stopWorking]').should('have.attr', 'aria-invalid', 'true');
+  });
+
+  it('conditionally validates when the patient stopped working if stopped working is yes', () => {
+    cy.visit('/en/relationship');
+
+    // field should be hidden to start
+    cy.get('[data-cy=stopWorkingWhen]').should('not.be.visible');
+
+    // field should be visible if Yes is selected
+    cy.get('[id=stopWorking1]').check();
+    cy.get('[data-cy=stopWorkingWhen]').should('be.visible');
+
+    // Conditional validation - since stopWorking is yes, we should see an error
+    cy.get('[type=submit]').click();
+    cy.get('#content .error-list').contains('When they stopped working is required');
+    cy.get('[name=stopWorkingWhen]').should('have.attr', 'aria-invalid', 'true');
+
+    // Check visibility of field with stopWorking set to no
+    cy.visit('/en/relationship');
+    cy.get('[data-cy=stopWorkingWhen]').should('not.be.visible');
+    cy.get('[id=stopWorking2]').check();
+    cy.get('[data-cy=stopWorkingWhen]').should('not.be.visible');
+
+    // Conditional validation - since stopWorking is no, we shouldn't see the error
+    cy.get('[type=submit]').click();
+    cy.get('#content .error-list').contains('When they stopped working is required').should('not.exist');
+
+    // Check visibility of field with stopWorking set to Did not discuss
+    cy.visit('/en/relationship');
+    cy.get('[data-cy=stopWorkingWhen]').should('not.be.visible');
+    cy.get('[id=stopWorking3]').check();
+    cy.get('[data-cy=stopWorkingWhen]').should('not.be.visible');
+
+    // Conditional validation - since stopWorking is Did not discuss, we shouldn't see the error
+    cy.get('[type=submit]').click();
+    cy.get('#content .error-list').contains('When they stopped working is required').should('not.exist');
+  });
+});
